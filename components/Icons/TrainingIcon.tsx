74e4fad10ca45d179cba38cc111c05192a19b3ae
Jason-1 <jason722698@gmail.com>
2025-04-09 12:37:38 +1200

    Replaced nested ternary with switch statement

--- components/Icons/TrainingIcon.tsx
+++ components/Icons/TrainingIcon.tsx
@@ -5,21 +5,26 @@ interface TrainingIconProps {
 }
 
 const TrainingIcon: React.FC<TrainingIconProps> = ({ trainingLevel }) => {
+  const calculateColour = () => {
+    switch (trainingLevel) {
+      case "U":
+        return "border-gray-500 bg-gray-600";
+      case "T":
+        return "border-blue-500 bg-blue-600";
+      case "E":
+        return "border-purple-500 bg-purple-700";
+      case "M":
+        return "border-yellow-500 bg-yellow-600";
+      case "L":
+        return "border-red-500 bg-red-600";
+      default:
+        return "";
+    }
+  };
+
   return (
     <span
-      className={`border px-2 rounded-full h-6 w-6 flex items-center justify-center ${
-        trainingLevel === "U"
-          ? "border-gray-500 bg-gray-600"
-          : trainingLevel === "T"
-          ? "border-blue-500 bg-blue-600"
-          : trainingLevel === "E"
-          ? "border-purple-500 bg-purple-700"
-          : trainingLevel === "M"
-          ? "border-yellow-500 bg-yellow-600"
-          : trainingLevel === "L"
-          ? "border-red-500 bg-red-600"
-          : ""
-      } `}
+      className={`border px-2 rounded-full h-6 w-6 flex items-center justify-center ${calculateColour()}`}
     >
       {trainingLevel}
     </span>

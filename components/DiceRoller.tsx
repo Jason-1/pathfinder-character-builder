bd1e2957bbb4dfef22e7f0e6fff2667275fe4f81
Jason-1 <jason722698@gmail.com>
2025-04-11 15:53:09 +1200

    DC UI only shows up when rolling a d20

--- components/DiceRoller.tsx
+++ components/DiceRoller.tsx
@@ -27,6 +27,7 @@ import { Input } from "./ui/input";
 interface DiceRollerProps {
   diceType: diceTypes;
   modifier: number;
+  diceCount?: number;
 }
 import {
   Popover,
@@ -35,7 +36,11 @@ import {
 } from "@/components/ui/popover";
 import { PopoverClose } from "@radix-ui/react-popover";
 
-const DiceRoller: React.FC<DiceRollerProps> = ({ diceType, modifier }) => {
+const DiceRoller: React.FC<DiceRollerProps> = ({
+  diceType,
+  modifier,
+  diceCount,
+}) => {
   const currentLevel = useSelector(
     (state: { level: { level: number } }) => state.level.level
   );
@@ -147,120 +152,124 @@ const DiceRoller: React.FC<DiceRollerProps> = ({ diceType, modifier }) => {
             <DialogTitle>
               <div className="flex justify-between items-center">
                 <span>Dice Roller</span>
-                <Popover>
-                  <PopoverTrigger className="font-normal">
-                    Current DC Level: {level}
-                  </PopoverTrigger>
-                  <PopoverContent>
-                    <div className="grid grid-cols-5 justify-between items-center text-center gap-1">
-                      <PopoverClose asChild>
-                        <Button
-                          variant={"outline"}
-                          className="flex justify-center items-center mt-1 col-span-5 cursor-pointer"
-                          onClick={() => handleLevelChange(0)}
-                        >
-                          Current Level
-                        </Button>
-                      </PopoverClose>
-                      {Array.from({ length: 20 }, (_, i) => (
-                        <PopoverClose key={i} asChild>
-                          <Button
-                            variant={"outline"}
-                            className="flex justify-center items-center mt-1 col-span-1 cursor-pointer"
-                            onClick={() => handleLevelChange(i + 1)}
-                          >
-                            {i + 1}
-                          </Button>
-                        </PopoverClose>
-                      ))}
+                {diceType === "d20" && (
+                  <>
+                    <Popover>
+                      <PopoverTrigger className="font-normal">
+                        Current DC Level: {level}
+                      </PopoverTrigger>
+                      <PopoverContent>
+                        <div className="grid grid-cols-5 justify-between items-center text-center gap-1">
+                          <PopoverClose asChild>
+                            <Button
+                              variant={"outline"}
+                              className="flex justify-center items-center mt-1 col-span-5 cursor-pointer"
+                              onClick={() => handleLevelChange(0)}
+                            >
+                              Current Level
+                            </Button>
+                          </PopoverClose>
+                          {Array.from({ length: 20 }, (_, i) => (
+                            <PopoverClose key={i} asChild>
+                              <Button
+                                variant={"outline"}
+                                className="flex justify-center items-center mt-1 col-span-1 cursor-pointer"
+                                onClick={() => handleLevelChange(i + 1)}
+                              >
+                                {i + 1}
+                              </Button>
+                            </PopoverClose>
+                          ))}
+                        </div>
+                      </PopoverContent>
+                    </Popover>
+                    <div className="flex items-center gap-2">
+                      <label className="font-normal">DC: </label>
+                      <Input
+                        type="number"
+                        value={DC}
+                        onChange={handleDCChange}
+                        className="w-40"
+                      />
                     </div>
-                  </PopoverContent>
-                </Popover>
-                <div className="flex items-center gap-2">
-                  <label className="font-normal">DC: </label>
-                  <Input
-                    type="number"
-                    value={DC}
-                    onChange={handleDCChange}
-                    className="w-40"
-                  />
-                </div>
-                <DropdownMenu>
-                  <DropdownMenuTrigger className="font-normal">
-                    DC Adjustment:{" "}
-                    {adjustment === "incrediblyEasy"
-                      ? "Incredibly Easy"
-                      : adjustment === "veryEasy"
-                      ? "Very Easy"
-                      : adjustment === "easy"
-                      ? "Easy"
-                      : adjustment === "hard"
-                      ? "Hard"
-                      : adjustment === "veryHard"
-                      ? "Very Hard"
-                      : adjustment === "incrediblyHard"
-                      ? "Incredibly Hard"
-                      : "Normal"}
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent>
-                    <DropdownMenuItem
-                      onClick={() => {
-                        setManualDC(false);
-                        setAdjustment("incrediblyEasy");
-                      }}
-                    >
-                      Incredibly Easy
-                    </DropdownMenuItem>
-                    <DropdownMenuItem
-                      onClick={() => {
-                        setManualDC(false);
-                        setAdjustment("veryEasy");
-                      }}
-                    >
-                      Very Easy
-                    </DropdownMenuItem>
-                    <DropdownMenuItem
-                      onClick={() => {
-                        setManualDC(false);
-                        setAdjustment("easy");
-                      }}
-                    >
-                      Easy
-                    </DropdownMenuItem>
-                    <DropdownMenuItem
-                      onClick={() => {
-                        setManualDC(false);
-                        setAdjustment("");
-                      }}
-                    >
-                      Normal
-                    </DropdownMenuItem>
-                    <DropdownMenuItem
-                      onClick={() => {
-                        setManualDC(false);
-                        setAdjustment("hard");
-                      }}
-                    >
-                      Hard
-                    </DropdownMenuItem>
-                    <DropdownMenuItem
-                      onClick={() => {
-                        setManualDC(false);
-                        setAdjustment("veryHard");
-                      }}
-                    >
-                      Very Hard
-                    </DropdownMenuItem>
-                    <DropdownMenuItem
-                      onClick={() => {
-                        setManualDC(false);
-                        setAdjustment("incrediblyHard");
-                      }}
-                    >
-                      Incredibly Hard
-                    </DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
+                    <DropdownMenu>
+                      <DropdownMenuTrigger className="font-normal">
+                        DC Adjustment:{" "}
+                        {adjustment === "incrediblyEasy"
+                          ? "Incredibly Easy"
+                          : adjustment === "veryEasy"
+                          ? "Very Easy"
+                          : adjustment === "easy"
+                          ? "Easy"
+                          : adjustment === "hard"
+                          ? "Hard"
+                          : adjustment === "veryHard"
+                          ? "Very Hard"
+                          : adjustment === "incrediblyHard"
+                          ? "Incredibly Hard"
+                          : "Normal"}
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent>
+                        <DropdownMenuItem
+                          onClick={() => {
+                            setManualDC(false);
+                            setAdjustment("incrediblyEasy");
+                          }}
+                        >
+                          Incredibly Easy
+                        </DropdownMenuItem>
+                        <DropdownMenuItem
+                          onClick={() => {
+                            setManualDC(false);
+                            setAdjustment("veryEasy");
+                          }}
+                        >
+                          Very Easy
+                        </DropdownMenuItem>
+                        <DropdownMenuItem
+                          onClick={() => {
+                            setManualDC(false);
+                            setAdjustment("easy");
+                          }}
+                        >
+                          Easy
+                        </DropdownMenuItem>
+                        <DropdownMenuItem
+                          onClick={() => {
+                            setManualDC(false);
+                            setAdjustment("");
+                          }}
+                        >
+                          Normal
+                        </DropdownMenuItem>
+                        <DropdownMenuItem
+                          onClick={() => {
+                            setManualDC(false);
+                            setAdjustment("hard");
+                          }}
+                        >
+                          Hard
+                        </DropdownMenuItem>
+                        <DropdownMenuItem
+                          onClick={() => {
+                            setManualDC(false);
+                            setAdjustment("veryHard");
+                          }}
+                        >
+                          Very Hard
+                        </DropdownMenuItem>
+                        <DropdownMenuItem
+                          onClick={() => {
+                            setManualDC(false);
+                            setAdjustment("incrediblyHard");
+                          }}
+                        >
+                          Incredibly Hard
+                        </DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </>
+                )}
                 <Button onClick={rollDice}>Roll Dice</Button>
               </div>
             </DialogTitle>
@@ -283,14 +292,16 @@ const DiceRoller: React.FC<DiceRollerProps> = ({ diceType, modifier }) => {
                   <span className="mr-1">+ {modifier} =</span>
                   <span
                     className={`${
-                      calculateSuccessLevel(result) === "Critical Failure"
-                        ? "text-red-700"
-                        : calculateSuccessLevel(result) === "Failure"
-                        ? "text-red-400"
-                        : calculateSuccessLevel(result) === "Success"
-                        ? "text-blue-500"
-                        : calculateSuccessLevel(result) === "Critical Success"
-                        ? "text-green-500"
+                      diceType === "d20"
+                        ? calculateSuccessLevel(result) === "Critical Failure"
+                          ? "text-red-700"
+                          : calculateSuccessLevel(result) === "Failure"
+                          ? "text-red-400"
+                          : calculateSuccessLevel(result) === "Success"
+                          ? "text-blue-500"
+                          : calculateSuccessLevel(result) === "Critical Success"
+                          ? "text-green-500"
+                          : ""
                         : ""
                     }`}
                   >

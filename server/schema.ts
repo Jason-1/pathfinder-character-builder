85ea6d58e05655eb02e0413130984680f5aa9d31
Jason-1 <jason722698@gmail.com>
2025-06-05 16:40:17 +1200

    Classes table schema

--- server/schema.ts
+++ server/schema.ts
@@ -18,7 +18,7 @@ export const characters = pgTable("characters", {
     .default("Unarmoured"),
 });
 
-export const ArnourCategoryEnum = pgEnum("category", [
+export const ArmourCategoryEnum = pgEnum("category", [
   "unarmoured",
   "light",
   "medium",
@@ -27,7 +27,7 @@ export const ArnourCategoryEnum = pgEnum("category", [
 
 export const armour = pgTable("armour", {
   name: varchar("name", { length: 255 }).unique().notNull().primaryKey(),
-  category: ArnourCategoryEnum("category").notNull().default("unarmoured"),
+  category: ArmourCategoryEnum("category").notNull().default("unarmoured"),
   ACBonus: integer("AC_bonus").notNull().default(0),
   dexCap: integer("dex_cap").notNull().default(5),
   strength: integer("strength").notNull().default(0),
@@ -48,3 +48,101 @@ export const CharacterRelations = relations(characters, ({ one }) => ({
 export const ArmourRelations = relations(armour, ({ many }) => ({
   characters: many(characters),
 }));
+
+export const TrainingEnum = pgEnum("training", [
+  "Untrained",
+  "Trained",
+  "Expert",
+  "Master",
+  "Legendary",
+]);
+
+export const AttributesEnum = pgEnum("attributes", [
+  "Strength",
+  "Dexterity",
+  "Constitution",
+  "Intelligence",
+  "Wisdom",
+  "Charisma",
+  "Free",
+]);
+
+export const classes = pgTable("classes", {
+  name: varchar("name", { length: 255 }).notNull().primaryKey(),
+  type: varchar("type", { length: 255 }).notNull(),
+  tradition: varchar("tradition", { length: 255 }).default(""),
+  perception: TrainingEnum("perception").notNull().default("Untrained"),
+  specialisation: integer("specialisation").array().notNull().default([]),
+  DC: TrainingEnum("DC").notNull().default("Untrained"),
+  hp: integer("hp").notNull().default(0),
+  attributes: AttributesEnum("attributes").array().notNull().default([]),
+  description: text("description")
+    .notNull()
+    .default("No description available"),
+});
+
+/*
+export type ClassType = {
+  name: string;
+  type: string;
+  tradition?: string;
+  perception: TrainingType;
+  specialisation: number[];
+  DC: TrainingType;
+  hp: number;
+  Attributes: AttributesType[];
+  description: string;
+};
+
+
+
+  saves: {
+    fortitude: number[];
+    reflex: number[];
+    will: number[];
+  };
+
+skills: {
+    Acrobatics?: TrainingType;
+    Arcana?: TrainingType;
+    Athletics?: TrainingType;
+    Crafting?: TrainingType;
+    Deception?: TrainingType;
+    Diplomacy?: TrainingType;
+    Intimidation?: TrainingType;
+    Medicine?: TrainingType;
+    Nature?: TrainingType;
+    Occultism?: TrainingType;
+    Performance?: TrainingType;
+    Religion?: TrainingType;
+    Society?: TrainingType;
+    Stealth?: TrainingType;
+    Survival?: TrainingType;
+    Thievery?: TrainingType;
+    additional: number;
+  };
+
+   attacks: {
+    simple: number[];
+    martial: number[];
+    advanced: number[];
+    unarmed: number[];
+  };
+
+  defences: {
+    unarmoured: number[];
+    light: number[];
+    medium: number[];
+    heavy: number[];
+  };
+
+ features: {
+    name: string;
+    level: number;
+    description: string;
+  }[];
+
+spells: spellObjectType[];
+
+
+*/

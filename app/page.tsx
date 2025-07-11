1de1e0f2b39bc5f286f474a0a092b4a0e108e366
Jason-1 <jason722698@gmail.com>
2025-07-11 12:30:11 +1200

    Character selection dialog re-added. Checking characters are loaded before displaying them

--- app/page.tsx
+++ app/page.tsx
@@ -34,11 +34,28 @@ import { setBackgroundData } from "./redux/Slices/data/backgroundDataSlice";
 import { setClassData } from "./redux/Slices/data/classDataSlice";
 import { setSubclassData } from "./redux/Slices/data/subclassDataSlice";
 import { setArmourData } from "./redux/Slices/data/armourDataSlice";
+import { createCharacter } from "@/server/actions/create-character";
+import { toast } from "sonner";
+import { setId } from "./redux/Slices/idSlice";
+
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { deleteCharacter } from "@/server/actions/delete-character";
 
 export default function Home() {
   const router = useRouter();
   const dispatch = useDispatch();
   const id = useSelector(selectID);
+
+  const [highlightedCharacter, setHighlightedCharacter] = useState<
+    number | null
+  >(null);
 
   const ancestries = useSelector(selectAncestryData);
   const ancestriesLoaded = useSelector(selectAncestryDataLoaded);
@@ -86,14 +103,13 @@ export default function Home() {
     },
   });
 
-  const { execute: fetchBackgrounds, isExecuting: backgroundsLoading } =
-    useAction(getBackgrounds, {
-      onSuccess: (data) => {
-        if (data.data) {
-          dispatch(setBackgroundData(data.data));
-        }
-      },
-    });
+  const { execute: fetchBackgrounds } = useAction(getBackgrounds, {
+    onSuccess: (data) => {
+      if (data.data) {
+        dispatch(setBackgroundData(data.data));
+      }
+    },
+  });
 
   const { execute: fetchClasses } = useAction(getClasses, {
     onSuccess: (data) => {
@@ -158,6 +174,20 @@ export default function Home() {
     fetchArmour();
   }, []);
 
+  const { execute: createCharacterExecute } = useAction(createCharacter, {
+    onSuccess: (data) => {
+      if (data.data) {
+        toast.success(`New character created`);
+        dispatch(setId(data.data.id));
+        router.push("/character-builder");
+      }
+    },
+  });
+
+  const handleSetID = (id: number) => {
+    dispatch(setId(id));
+  };
+
   const getAncestryData = (ancestryName: string) => {
     return ancestries.find((ancestry) => ancestry.name === ancestryName);
   };
@@ -171,57 +201,117 @@ export default function Home() {
     return classes.find((cls) => cls.name === className);
   };
 
+  const { execute: deleteCharacterExecute } = useAction(deleteCharacter, {
+    onSuccess: (data) => {
+      if (data.data) {
+        toast.success(`Character "${data.data.name}" deleted successfully!`);
+        router.push("/");
+      }
+    },
+  });
+
   return (
     <main className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-3xl font-bold mb-8 text-center">
         Welcome to Pathfinder Character Builder
       </h1>
 
-      {isLoading ? (
-        <div>Loading characters...</div>
-      ) : (
-        <ul className="space-y-4">
-          {characters.map((character) => {
-            const ancestryData = getAncestryData(character.ancestryName);
-            const heritageData = getHeritageData(character.heritageName);
-            const classData = getClassData(character.className);
-            return (
-              <li key={character.id} className="border p-4 rounded">
-                <div className="font-bold">
-                  {character.name} (Level {character.level})
+      <Dialog>
+        <DialogTrigger>
+          <span className="mt-2 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2">
+            Load existing character
+          </span>
+        </DialogTrigger>
+        <DialogContent className="w-full max-w-full md:w-1/3 h-3/4 max-h-[75vh] flex flex-col">
+          <DialogHeader>
+            <DialogTitle>Select a character to load</DialogTitle>{" "}
+          </DialogHeader>
+          <div className="h-full overflow-y-auto border">
+            {isLoading ? (
+              <div>Loading Characters...</div>
+            ) : characters.length === 0 ? (
+              <div className="flex items-center justify-center h-full">
+                <div className="text-center text-gray-500">
+                  No characters found. Create your first character!
                 </div>
+              </div>
+            ) : (
+              <ul>
+                {characters.map((char) => (
+                  <li
+                    className={`${
+                      highlightedCharacter === char.id
+                        ? "bg-gray-400"
+                        : "hover:bg-gray-800"
+                    } cursor-pointer `}
+                    key={char.id}
+                    onClick={() => {
+                      handleSetID(char.id);
+                      setHighlightedCharacter(char.id);
+                    }}
+                    onDoubleClick={() => {}}
+                  >
+                    <span className="select-none">
+                      {char.name || "Unnamed Adventurer"}: Level: {char.level}{" "}
+                      {char.ancestryName} {char.className}
+                    </span>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
 
-                <div>Ancestry: {character.ancestryName}</div>
-
-                {ancestryData && (
-                  <div className="mt-2">
-                    <div>Ancestry HP: {ancestryData.hp}</div>
-                    <div>Size: {ancestryData.size}</div>
-                  </div>
-                )}
-
-                {heritageData && (
-                  <div className="mt-2">
-                    <div>Heritage: {heritageData.name}</div>
-                  </div>
-                )}
-
-                <div>Class: {character.className}</div>
-
-                {/* Display class attributes */}
-                {classData && (
-                  <div className="mt-2">
-                    <div>Hit Points: {classData.hp}</div>
-                  </div>
-                )}
-              </li>
-            );
-          })}
-        </ul>
-      )}
+          <div className="flex flex-row gap-4 items-center justify-center w-full">
+            <Button className="w-full" onClick={() => {}} disabled={!id}>
+              Confirm Selection
+            </Button>
+            <Dialog>
+              <DialogTrigger asChild>
+                <span className="cursor-pointer inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90 h-9 px-4 py-2">
+                  Delete Character
+                </span>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>
+                    Are you sure you want to delete{" "}
+                    {characters.find((char) => char.id === id)?.name ||
+                      "that which has no name"}
+                    ?
+                  </DialogTitle>
+                </DialogHeader>
+                <div className="flex flex-row gap-4 items-center justify-center w-full">
+                  <DialogClose asChild>
+                    <span
+                      className="cursor-pointer w-full inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90 h-9 px-4 py-2"
+                      onClick={() => {
+                        id && deleteCharacterExecute({ id });
+                        fetchCharacters();
+                      }}
+                    >
+                      Yes
+                    </span>
+                  </DialogClose>
+                  <DialogClose asChild>
+                    <span className="cursor-pointer w-full inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2">
+                      No
+                    </span>
+                  </DialogClose>
+                </div>
+              </DialogContent>
+            </Dialog>
+          </div>
+        </DialogContent>
+      </Dialog>
 
       <div className="flex flex-col gap-2 mt-8">
-        <Button onClick={() => {}}>Create new character</Button>
+        <Button
+          onClick={() => {
+            createCharacterExecute({ name: "" });
+          }}
+        >
+          Create new character
+        </Button>
       </div>
     </main>
   );
